8fb407681f7ba2a0a5b55e06cb7061f43cc4f0e4
faithger <faith@localhost.localdomain>
2026-01-06 14:50:25 +0100

    Struc exist

--- src/TreeLogic.asm
+++ src/TreeLogic.asm
@@ -1,8 +1,17 @@
 SECTION .bss
 
+    struc TreeNode
+        .ParentAdress: resq 1 ; Node parent
+        .Value1 resq: 1       ; Value or child adress
+        .Value2 resq: 1       ; Value or child adress
+        .Flags resb: 1        ; Flags for operation (2bits), Flags for if Value1 and 2 are adress or number
+    endstruc 
+
 SECTION .data
 
 SECTION .text
+
+CreateNode:
 
 global _start
 
